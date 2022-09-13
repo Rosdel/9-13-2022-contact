1ffdb5055ff09514671e44b66d42db1991867122
Akora Solomon <35068415+akoraSolomon@users.noreply.github.com>
2022-09-13 17:35:15 +0300

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'g4g';
+export class AppComponent implements OnInit{
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+  }
+  signupForm!:FormGroup;
+  listData:any;
+
+  constructor(private fb:FormBuilder){
+
+    this.listData = [];
+
+    this.signupForm=this.fb.group({
+      firstname:['',Validators.required],
+      lastname:['',Validators.required],
+      age:['',Validators.required],
+      phone:['',Validators.required],
+    })
+  }
+
+  public AddUser(): void{
+    this.listData.push(this.signupForm.value)
+    this.signupForm.reset();
+  }
+  Reset(){
+    this.signupForm.reset()
+  }
+  RemoveItem(index: any): void{ 
+    this.listData.splice(index, 1)
+}
+
+
+  ngOnInit(){
+
+  }
 }
